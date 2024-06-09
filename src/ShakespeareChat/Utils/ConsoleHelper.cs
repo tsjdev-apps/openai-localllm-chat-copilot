c338438eb7ef944bfd4e6b3b0e734dad180105ec
Sebastian Jensen <apps@tsjdev-apps.de>
2024-06-09 22:22:22 +0200

    Create ConsoleHelper.cs

---
+++ src/ShakespeareChat/Utils/ConsoleHelper.cs
@@ -0,0 +1,76 @@
+﻿using Spectre.Console;
+
+namespace ShakespeareChat.Utils;
+
+internal static class ConsoleHelper
+{
+    /// <summary>
+    ///     Clears the console and creates the header for the application.
+    /// </summary>
+    public static void ShowHeader()
+    {
+        AnsiConsole.Clear();
+
+        Grid grid = new();
+        grid.AddColumn();
+        grid.AddRow(new FigletText("OpenAI .NET Demo").Centered().Color(Color.Red));
+        grid.AddRow(Align.Center(new Panel("[red]Sample by Thomas Sebastian Jensen ([link]https://www.tsjdev-apps.de[/])[/]")));
+
+        AnsiConsole.Write(grid);
+        AnsiConsole.WriteLine();
+    }
+
+    /// <summary>
+    ///     Displays a prompt with the provided options and returns the selected option.
+    /// </summary>
+    /// <param name="options">The list of options to choose from.</param>
+    /// <returns>The selected option.</returns>
+    public static string SelectFromOptions(List<string> options)
+    {
+        ShowHeader();
+
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("Select from the following [yellow]options[/]?")
+            .AddChoices(options));
+    }
+
+    /// <summary>
+    ///     Displays a prompt with the provided message and returns the user input.
+    /// </summary>
+    /// <param name="prompt">The prompt message.</param>
+    /// <returns>The user input.</returns>
+    public static string GetString(string prompt)
+    {
+        ShowHeader();
+
+        return AnsiConsole.Prompt(
+            new TextPrompt<string>(prompt)
+            .PromptStyle("white")
+            .ValidationErrorMessage("[red]Invalid prompt[/]")
+            .Validate(prompt =>
+            {
+                if (prompt.Length < 3)
+                {
+                    return ValidationResult.Error("[red]Value too short[/]");
+                }
+
+                if (prompt.Length > 200)
+                {
+                    return ValidationResult.Error("[red]Value too long[/]");
+                }
+
+                return ValidationResult.Success();
+            }));
+    }
+
+    /// <summary>
+    ///     Writes the specified text to the console.
+    /// </summary>
+    /// <param name="text">The text to write.</param>
+    public static void WriteToConsole(string text)
+    {
+        AnsiConsole.Markup($"[white]{text}[/]");
+    }
+}
+
